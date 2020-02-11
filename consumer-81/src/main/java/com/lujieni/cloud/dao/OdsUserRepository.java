f948e9e1e2e896ab3a6fb35a9a58e4ed09227146
admin <982584989@qq.com>
2020-02-11 19:36:01 +0800

    代码提交

---
+++ consumer-81/src/main/java/com/lujieni/cloud/dao/OdsUserRepository.java
@@ -0,0 +1,8 @@
+package com.lujieni.cloud.dao;
+
+import com.lujieni.cloud.entity.OdsUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OdsUserRepository extends JpaRepository<OdsUser,Long> {
+
+}
